10f2e38a8372f44ef854b1df529ac6203adbecf6
jaysonmulwa <jayson.mulwa@gmail.com>
2022-12-18 11:13:05 +0300

    fix: typo

--- src/tests/unit/currencies.test.ts
+++ src/tests/unit/currencies.test.ts
@@ -1,29 +1,29 @@
 describe("currencies", () => {
-    it('should_create_curencies', ()=>{
+    it('should_create_currencies', ()=>{
         const one: number = 1;
         const two: number = 1;
         expect(one).toEqual(two);
     })
 
-    it('should_edit_curencies', ()=>{
+    it('should_edit_currencies', ()=>{
         const one: number = 1;
         const two: number = 1;
         expect(one).toEqual(two);
     })
 
-    it('should_delete_curencies', ()=>{
+    it('should_delete_currencies', ()=>{
         const one: number = 1;
         const two: number = 1;
         expect(one).toEqual(two);
     })
 
-    it('should_fetch_curencies_for_an_orgaisation', ()=>{
+    it('should_fetch_currencies_for_an_orgaisation', ()=>{
         const one: number = 1;
         const two: number = 1;
         expect(one).toEqual(two);
     })
 
-    it('should_search_curencies_for_an_orgaisation', ()=>{
+    it('should_search_currencies_for_an_orgaisation', ()=>{
         const one: number = 1;
         const two: number = 1;
         expect(one).toEqual(two);
