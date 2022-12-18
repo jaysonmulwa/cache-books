f0e4621dbd889221530287b37913721d2cf28681
jaysonmulwa <jayson.mulwa@gmail.com>
2022-12-18 11:13:19 +0300

    feat: create orgs test

---
+++ src/tests/unit/organization.test.ts
@@ -0,0 +1,25 @@
+describe("organizations", () => {
+    it('should_create_organizations', ()=>{
+        const one: number = 1;
+        const two: number = 1;
+        expect(one).toEqual(two);
+    })
+
+    it('should_edit_organizations', ()=>{
+        const one: number = 1;
+        const two: number = 1;
+        expect(one).toEqual(two);
+    })
+
+    it('should_delete_organizations', ()=>{
+        const one: number = 1;
+        const two: number = 1;
+        expect(one).toEqual(two);
+    })
+
+    it('should_search_organizations_for_an_orgaisation', ()=>{
+        const one: number = 1;
+        const two: number = 1;
+        expect(one).toEqual(two);
+    })
+});
